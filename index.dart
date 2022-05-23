5d743ae72184e256169b8ba5117fd9c4971329ee
Timus23 <me.summet@gmail.com>
2022-05-23 20:59:46 +0545

    multiplication param renamed

--- index.dart
+++ index.dart
@@ -10,8 +10,8 @@ void add(int value1, int value2) {
   print(value1 + value2);
 }
 
-void multiply(int value1, int value2) {
-  print(value1 * value2);
+void multiply(int val1, int val2) {
+  print(val1 * val2);
 }
 
 void subtract(int value1, int value2) {
